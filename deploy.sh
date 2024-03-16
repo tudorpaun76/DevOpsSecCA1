516eacbc172b24a61248d935cffcfb227f9dc1ac
Tudor <tudor.paun76@gmail.com>
2024-03-16 19:03:34 +0000

    pipeline fixes

--- deploy.sh
+++ deploy.sh
@@ -6,7 +6,10 @@ pm2 stop example_app
 
 cd DevOpsSecCA1/SimpleApplication/
 
-npm install
+npm install -g npm@latest   # Upgrade npm first
+npm cache clean -f          # Clean npm cache
+npm install -g n            # Install 'n' Node version manager
+n 14                        # Install Node.js version 14
 echo $PRIVATE_KEY > privatekey.pem
 echo $SERVER > server.crt
 
