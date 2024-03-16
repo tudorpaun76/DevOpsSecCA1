721f532e4f31199d9589de92cdb06c48e678d920
Tudor <tudor.paun76@gmail.com>
2024-03-16 21:56:20 +0000

    final commit

--- deploy.sh
+++ deploy.sh
@@ -1,12 +1,13 @@
 sudo apt update && sudo apt install nodejs npm
 
+npm install -g npm@latest
+
 sudo npm install -g pm2
 
 pm2 stop example_app
 
 cd DevOpsSecCA1/SimpleApplication/
 
-npm install
 echo $PRIVATE_KEY > privatekey.pem
 echo $SERVER > server.crt
 
