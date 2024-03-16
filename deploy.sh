c8ba3d1395587fa91734da1920ec21e4c21e5fef
Tudor <tudor.paun76@gmail.com>
2024-03-16 18:59:31 +0000

    update pipeline

--- deploy.sh
+++ deploy.sh
@@ -2,8 +2,6 @@ sudo apt update && sudo apt install nodejs npm
 
 sudo npm install -g pm2
 
-sudo npm install http-errors
-
 pm2 stop example_app
 
 cd DevOpsSecCA1/SimpleApplication/
@@ -13,5 +11,3 @@ echo $PRIVATE_KEY > privatekey.pem
 echo $SERVER > server.crt
 
 pm2 start ./bin/www --name example_app
-
-pm2 logs
